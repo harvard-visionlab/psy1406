5242a1a587316986b7eb1210c12032760159ae5d
grez72 <grez72@gmail.com>
2022-02-24 17:20:01 -0500

    initial commit

---
+++ 2022/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://www.dropbox.com/s/7tqlvb69lvx570h/BaldBear.jpg -q --show-progress -O /home/jovyan/images/BaldBear.jpg
+wget https://www.dropbox.com/s/lv6tjxuc90mksgc/BrownBear.jpg -q --show-progress -O /home/jovyan/images/BrownBear.jpg
+wget https://www.dropbox.com/s/65p68g331kby809/Gorilla.jpg -q --show-progress -O /home/jovyan/images/Gorilla.jpg
+wget https://www.dropbox.com/s/be1hkifaz8u04y9/DiCaprio_Anchor.jpg -q --show-progress -O /home/jovyan/images/DiCaprio_Anchor.jpg
+wget https://www.dropbox.com/s/xn3y46bpccopdl7/DiCaprio_HardNegative.jpg -q --show-progress -O /home/jovyan/images/DiCaprio_HardNegative.jpg
+wget https://www.dropbox.com/s/8londclzzyj3oji/DiCaprio_NegativeClooney.jpg -q --show-progress -O /home/jovyan/images/DiCaprio_NegativeClooney.jpg
+wget https://www.dropbox.com/s/ddlfya3368jdhci/DiCaprio_Positive.jpg -q --show-progress -O /home/jovyan/images/DiCaprio_Positive.jpg
